f3ae54af410fd032c1f1ef295f236d00a1afd7e8
MarieDechaux <marie.dechaux@gmail.com>
2020-10-25 18:22:09 +0100

    all works

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,20 @@ require 'faker'
   City.create(city_name: Faker::Address.city)
 end
 
-15.times do   
-  Dogsitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: City.all.sample)
-  Dog.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, city: City.all.sample)
+15.times do  
+  paris =  City.all.sample
+  Dogsitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: paris)
+  Dog.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, city: paris)
 end
 
 25.times do
   dog = Dog.all.sample
-  Stroll.create(
-  duration: Faker::Number.between(from: 15, to: 60),
-  has_played: [true, false].sample,
-  dogsitter: Dogsitter.all.sample,
-  dog: dog,
-  city: dog.city)
+  if Dogsitter.all.sample.city == dog.city
+    Stroll.create(
+    duration: Faker::Number.between(from: 15, to: 60),
+    has_played: [true, false].sample,
+    dogsitter: Dogsitter.all.sample,
+    dog: dog,
+    city: dog.city)
+  end
 end
